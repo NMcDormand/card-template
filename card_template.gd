e3ae679880f7faa9cb43d9726a8dfa0d52abc655
NMcDormand <158542342+NMcDormand@users.noreply.github.com>
2024-10-29 18:37:28 -0900

    deck manipulation

--- card_template.gd
+++ card_template.gd
@@ -2,9 +2,56 @@ extends Path2D
 @export var speed = 1
 @export var card_position: float
 var card_position_in_hand: int
+var hovered: bool
+var selected = false
+var state: Callable = state_undefined
+
 
 
 func _process(delta):
 	if card_position > get_child(0).progress_ratio:
 		get_child(0).progress_ratio = get_child(0).progress_ratio + speed * delta
+	state.call(delta)
 
+
+func _on_area_2d_mouse_entered() -> void:
+	if state != state_selected:
+		state = state_hovered
+
+
+func _on_area_2d_mouse_exited() -> void:
+	if state != state_selected:
+		state = state_not_hovered
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		var mouse_event = event as InputEventMouseButton
+		if mouse_event.button_index == MOUSE_BUTTON_LEFT and mouse_event.pressed:
+			if state == state_hovered:
+				state = state_selected
+				get_parent().get_parent().selected_group.append(self)
+
+
+func state_hovered(delta):
+	if get_child(0).get_child(0).get_child(0).progress_ratio < 0.5:
+		get_child(0).get_child(0).get_child(0).progress_ratio = get_child(0).get_child(0).get_child(0).progress_ratio + speed*3 * delta
+	modulate.r = 0.9
+	modulate.g = 0.9
+	modulate.b = 0.9
+
+
+func state_not_hovered(delta):
+	if get_child(0).get_child(0).get_child(0).progress_ratio > 0:
+		get_child(0).get_child(0).get_child(0).progress_ratio = get_child(0).get_child(0).get_child(0).progress_ratio - speed*3 * delta
+	modulate.r = 0.8
+	modulate.g = 0.8
+	modulate.b = 0.8
+
+
+func state_selected(delta):
+	get_child(0).get_child(0).get_child(0).progress_ratio = 1
+
+
+func state_undefined(delta):
+	pass
--- deck management.gd
+++ deck management.gd
@@ -1,25 +1,23 @@
 extends Node
 var Deck: Array
 var Hand: Array
+var Hand_Mod: Array
 var Discard: Array
 var To_be_removed: Array
 var Card_positions: Array
 @export var card_distance_scaler: float
-@export var Hand_size: int
+var selected_group: Array
+
 
 var card_template = preload("res://card_template.tscn")
 
 
 func _ready():
-	for i in Hand_size:
-		Hand.append(0)
-	_hand_size_update()
-	for i in Hand_size:
+	for i in 10:
 		var card_instance = card_template.instantiate()
-		card_instance.card_position = Card_positions[i]
-		card_instance.position.y = -1654.81
+		card_instance.visible = false
+		Deck.append(card_instance)
 		get_child(0).add_child(card_instance)
-
 
 
 #shuffles the Deck. 
@@ -33,26 +31,54 @@ func _shuffle():
 func _draw(draw_count):
 	for i in draw_count:
 		if Deck.size() > 0:
-			Hand.append(Deck[0])
+			Hand_Mod.append(Deck[0])
+			Deck[0].visible = true
+			Deck[0].set_process(true)
 			Deck.remove_at(0)
-			call(_hand_size_update())
+			_hand_size_update()
 		else:
 			if Discard.size() == 0:
 				pass
 			Deck.append_array(Discard)
 			Discard.clear()
-			call(_shuffle())
-			Hand.append(Deck[0])
+			_shuffle()
+			Hand_Mod.append(Deck[0])
+			Deck[0].visible = true
+			Deck[0].set_process(true)
 			Deck.remove_at(0)
-			call(_hand_size_update())
+			_hand_size_update()
 
 
 #takes all cards in the to_be_removed array and puts them into the discard
 func _discard(To_be_removed):
 	Discard.append_array(To_be_removed)
-	call(_hand_size_update())
+	for i in To_be_removed.size():
+		Hand.erase(To_be_removed[i])
+	for i in Discard.size():
+		Discard[i].visible = false
+		Discard[i].set_process(false)
+		Discard[i].get_child(0).progress_ratio = 0
+	_hand_size_update()
+	To_be_removed.clear()
+	
 
 #creates an array that gives the positions each card in your hand should sit
 func _hand_size_update():
-	for i in Hand.size():
-		Card_positions.append( 0.5 + (i - (Hand_size - 1) / 2) * card_distance_scaler)
+	Hand_Mod.append_array(Hand)
+	Card_positions.clear()
+	for i in Hand_Mod.size():
+		Card_positions.append( 0.5 + (i - (Hand_Mod.size() - 1) / 2) * card_distance_scaler)
+	for i in Hand_Mod.size():
+			Hand_Mod[i].card_position = Card_positions[i]
+	Hand.clear()
+	Hand.append_array(Hand_Mod)
+	Hand_Mod.clear()
+
+
+func _on_draw_pressed() -> void:
+	_draw(1)
+
+
+func _on_discard_pressed() -> void:
+	if selected_group.size() > 0:
+		_discard(selected_group)
